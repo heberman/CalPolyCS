6cda05de83f355602c9cc349e294a6211d602743
heberman <71403435+heberman@users.noreply.github.com>
2022-10-26 13:27:30 -0700

    Delete cpe101 directory

--- cpe101/lab8/file/file.py
+++
@@ -1,9 +0,0 @@
-def main():
-    f = open("in.txt", "r")
-    i = 0
-    for line in f:
-        print("Line", i, "(" + str(len(line)), "chars): " + line, end='')
-        i += 1
-
-if __name__ == '__main__':
-    main()
--- cpe101/lab8/funcs_objects/funcs_objects.py
+++
@@ -1,8 +0,0 @@
-from objects import *
-from math import sqrt
-
-def distance(p1, p2):
-    return sqrt((p2.x - p1.x)**2 + (p2.y - p1.y)**2)
-
-def circles_overlap(c1, c2):
-    return distance(c1.center, c2.center) < (c1.radius + c2.radius)
